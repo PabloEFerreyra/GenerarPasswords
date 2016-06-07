7420f41699ce5337d27c7091e2c2c9ab46e8c15a
hackerzeta <pabloeferreyra@gmail.com>
2016-06-07 16:49:55 -0300

    Cambio para eliminar la necesidad de pre crear un archivo, elimina el mismo archivo al salir, y se redujo la cadena a 25 caracteres

--- GenerarPasswords/Form1.Designer.cs
+++ GenerarPasswords/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 
 namespace GenerarPasswords
@@ -39,36 +42,39 @@ namespace GenerarPasswords
             // textBox1
             // 
             this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(12, 169);
-            this.textBox1.MaxLength = 50;
+            this.textBox1.MaxLength = 25;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(376, 20);
+            this.textBox1.Size = new System.Drawing.Size(978, 75);
             this.textBox1.TabIndex = 0;
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress);
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.Location = new System.Drawing.Point(12, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(138, 159);
             this.button1.TabIndex = 1;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 301);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1002, 301);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Generador De Claves";
-            this.Load += new System.EventHandler(this.button1_Click);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- GenerarPasswords/Form1.cs
+++ GenerarPasswords/Form1.cs
@@ -1,87 +1,114 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 namespace GenerarPasswords
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+    
 
-        private void button1_Click(object sender, EventArgs e)
+public partial class Form1 : Form
+{
+        
+    static string Path = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") +
+                                        "\\Documents\\lineas.txt";
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        var password = NewPassword.GenerarPass();
+        textBox1.Text = password;
+    }
+
+    public class NewPassword
+    {
+
+        private static char[] _valueAfanumeric =
+        {
+            'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd',
+            'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I',
+            'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '!', '#', '$',
+            '%', '&', '?', '¿'
+        };
+
+        public static string GenerarPass()
+        {
+            string[] _lines = File.ReadAllLines(Path);
+            Random ram = new Random();
+            int logitudPass = ram.Next(25, 25);
+            string password = String.Empty;
+
+            for (int i = 0; i < logitudPass; i++)
+            {
+                int rm = ram.Next(0, 2);
+
+                if (rm == 0)
+                {
+                    password += ram.Next(0, 10);
+                }
+                else
+                {
+                    password += _valueAfanumeric[ram.Next(0, 59)];
+                }
+            }
+            foreach (string line in _lines)
+            {
+                if (line == password)
+                {
+                    GenerarPass();
+                }
+                else
+                {
+                    using (StreamWriter file = new StreamWriter(Path, true))
+                    {
+                        file.WriteLine(password+',');
+                    }
+                    return password;
+                }
+            }
+            using (StreamWriter file = new StreamWriter(Path, true))
+            {
+                file.WriteLine(password+',');
+            }
+            return password;
+        }
+    }
+
+
+    private void button1_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (e.KeyChar == (int) Keys.Enter)
         {
             var password = NewPassword.GenerarPass();
             textBox1.Text = password;
         }
-
-        public class NewPassword
-        {
-            private static string Path = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") +
-                                         "\\Documents\\lineas.txt";
-
-            private static string[] lines = File.ReadAllLines(@Path);
-
-            private static char[] ValueAfanumeric =
-            {
-                'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd',
-                'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I',
-                'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '!', '#', '$',
-                '%', '&', '?', '¿'
-            };
-
-            public static string GenerarPass()
-            {
-                Random ram = new Random();
-                int logitudPass = ram.Next(50, 50);
-                string password = String.Empty;
-
-                for (int i = 0; i < logitudPass; i++)
-                {
-                    int rm = ram.Next(0, 2);
-
-                    if (rm == 0)
-                    {
-                        password += ram.Next(0, 10);
-                    }
-                    else
-                    {
-                        password += ValueAfanumeric[ram.Next(0, 59)];
-                    }
-                }
-                foreach (string line in lines)
-                {
-                    if (line == password)
-                    {
-                        GenerarPass();
-                    }
-                    else
-                    {
-                        using (StreamWriter file = new StreamWriter(@Path, true))
-                        {
-                            file.WriteLine(password);
-                        }
-                        return password;
-                    }
-                }
-                using (StreamWriter file = new StreamWriter(@Path, true))
-                {
-                    file.WriteLine(password);
-                }
-                return password;
-            }
-        }
-
-
-    private void button1_KeyPress(object sender, KeyPressEventArgs e)
-            {
-                if (e.KeyChar == (int) Keys.Enter)
-                {
-                    var password = NewPassword.GenerarPass();
-                    textBox1.Text = password;
-                }
-            }
-        }
     }
+
+    private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+    {
+        if(File.Exists(Path))
+                File.Delete(Path);
+    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var archivo = Path;
+            if (File.Exists(archivo))
+                File.Delete(archivo);
+            using (var fileStream = File.Create(archivo))
+            {
+                fileStream.Flush();
+            }
+
+            var password = NewPassword.GenerarPass();
+            textBox1.Text = password;
+
+        }
+
+}
+
+}
+
