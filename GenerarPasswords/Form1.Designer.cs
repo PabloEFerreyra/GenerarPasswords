55557eec1ed3096288f16682bc5f341c390b12d7
hackerzeta <pabloeferreyra@gmail.com>
2016-06-06 23:28:29 -0300

    Initial Commit, Generador de claves al azar

--- GenerarPasswords/Form1.Designer.cs
+++ GenerarPasswords/Form1.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Location = new System.Drawing.Point(12, 74);
+            this.textBox1.MaxLength = 50;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(376, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ayuda";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(400, 301);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Generador De Claves";
+            this.Load += new System.EventHandler(this.button1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- GenerarPasswords/Form1.cs
+++ GenerarPasswords/Form1.cs
@@ -16,5 +16,57 @@ namespace GenerarPasswords
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password ="";
+            password =NewPassword.GenerarPass();
+            textBox1.Text = password;
+        }
+
+        public class NewPassword
+        {
+            static char[] ValueAfanumeric = { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '!', '#', '$', '%', '&', '?', '¿' };
+
+            public static string GenerarPass()
+            {
+                Random ram = new Random();
+                int LogitudPass = ram.Next(50, 50);
+                string Password = String.Empty;
+
+                for (int i = 0; i < LogitudPass; i++)
+                {
+                    int rm = ram.Next(0, 2);
+
+                    if (rm == 0)
+                    {
+                        Password += ram.Next(0, 10);
+                    }
+                    else
+                    {
+                        Password += ValueAfanumeric[ram.Next(0, 59)];
+                    }
+                }
+                
+                return Password;
+
+            }
+        }
+
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (int) Keys.Enter)
+            {
+                string password = "";
+                password = NewPassword.GenerarPass();
+                textBox1.Text = password;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Ayuda Ayuda = new Ayuda();
+            Ayuda.Show();
+        }
     }
 }
